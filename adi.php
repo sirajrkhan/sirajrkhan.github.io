646413a43bee5cc9e50e311cf05608f1e9a44a7b
Siraj Khan <sirajrkhan@live.in>
2016-05-22 23:42:41 +0530

    Get method completed

--- adi.php
+++ adi.php
@@ -1,18 +1,11 @@
-<?php 
-	echo "Bismillah<br>"; 
-	
+<?php 	
 	#set up the GET method
 		# Assuming you are hitting some URL/Exe and that returns you an array with comma separated value;
 		# Something like this: 
-			#$devices = exec ('myExecFile_URL');
-		#so you have an array with all values in $devices
+			#$devices = exec ('myExecFileOrURL_thisShouldReturnArray');
+		#so you have an array with all values in $devices -- in below format -- NO EMPTY VALUES -- ORDER & ARRAY COUNT SHOULD NOT CHANGE!!
 		
-	$devices = array(
-		array('1','Fan','192.168.1.1','0'), 
-		array('2','AC','192.168.1.2','1'), 
-		array('3','Fridge','192.168.1.3','1'), 
-		array('4','LCD','192.168.1.4','0'), 
-		array('5','Computer','192.168.1.5','1')
+	$devices = array( array('1','Fan','192.168.1.1','0'), array('2','AC','192.168.1.2','1'), array('3','Fridge','192.168.1.3','1'), array('4','LCD','192.168.1.4','0'), array('5','Computer','192.168.1.5','0')
 	);
 	
 ?>
@@ -26,9 +19,6 @@
 	<title>Device Info</title>
 	<link rel="stylesheet" href="bootstrap.min.css">
 	<style>
-	.red{
-		background-color:red;
-	}
 	</style>
 </head>
 <body>
@@ -45,51 +35,24 @@
       <th scope="col">Status</th>
     </tr>
 	
-	<!-- Loop Starts /-->
-	
-<?php for ( $i =0; $i < count($devices); $i++){ ?>		
-    <tr>
-		  <td><?php echo $devices[$i][0]; ?></td>
-		  <td><?php echo $devices[$i][1]; ?></td>
-		  <td><?php echo $devices[$i][2]; ?></td>
-		  <td><label>
-			<input type="radio" name="status" value="On" id="status_0">
-			On</label>
-			<label>
-			  <input type="radio" name="status" value="Off" id="status_1" checked>
-		  Off</label></td>
-		  <td><p>Current Status: OFF</p></td>
-	</tr>	
-<?php }; ?>
-		
+	<!-- Loop Starts /-->	
+	<?php for ( $i =0; $i < count($devices); $i++){ ?>		
+		<tr>
+			  <td><?php echo $devices[$i][0]; ?></td>
+			  <td><?php echo $devices[$i][1]; ?></td>
+			  <td><?php echo $devices[$i][2]; ?></td>
+			  <td>
+			  <?php 
+				$isOn = ($devices[$i][3]=="1" ? "checked" : ""); 
+			    $isOff = ($devices[$i][3]=="0" ? "checked" : ""); 
+			  ?>
+				<label><input type="radio" name="status<?php echo $i; ?>" value="On"  id="status_<?php echo $i; ?>" <?php echo $isOn; ?>  >On</label>
+				<label><input type="radio" name="status<?php echo $i; ?>" value="Off" id="status_<?php echo $i; ?>" <?php echo $isOff; ?> >Off</label>
+			  </td>
+			  <td><p>Current Status (@Server): <?php echo($isOn=="")?"OFF":"ON"; ?> </p></td>
+		</tr>	
+	<?php }; ?>
 	<!-- Loop Ends /-->
-	
-	<!--
-    <tr>
-      <td>2</td>
-      <td>A.C.</td>
-      <td>192.168.1.2</td>
-      <td><label>
-        <input name="status2" type="radio" id="status_3" value="On" checked >
-        On</label>
-        <label>
-          <input type="radio" name="status2" value="Off" id="status_4">
-      Off</label></td>
-      <td><p>Current Status: ON</p></td>
-    </tr>
-    <tr>
-      <td>3</td>
-      <td>LED</td>
-      <td>192.168.1.3</td>
-      <td><label>
-        <input type="radio" name="status3" value="On" id="status_5" >
-        On</label>
-        <label>
-          <input type="radio" name="status3" value="Off" id="status_6" checked>
-      Off</label></td>
-      <td><p>Current Status: OFF</p></td>
-    </tr>
-	-->
 	
   </table>
   <div class="text-center">
